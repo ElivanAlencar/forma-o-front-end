54cf1abb9836126654085f3bb1634407237cd7ba
elivan-alencar <elivanalencar@hotmail.com>
2024-12-28 11:00:41 -0300

    scriptinfor

---
+++ JAVASCRIPT-INFO/operadores/js/script.js
@@ -0,0 +1,42 @@
+// number
+
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -28);
+
+
+// operações aritméticas
+
+console.log(2 + 7);
+console.log(7 - 2);
+
+// operadores de divição
+
+console.log( 10 / 2);
+console.log(20 / 3);
+
+// operadores de multiplicação
+
+console.log(5 * 10);
+console.log(10 * 15);
+
+
+// dois operadores juntos
+
+console.log( 5 + (4 * 2));
+
+
+// especial number
+
+/* special number são dados considerados como números, mas não funionam entre  eles. os valoes são: infinity, -infinity, nan (not a number). algumas operações podem resultar nestes valores; vamos ver na prática*/
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(typeof NaN);
+
+// strings texto
+
+console.log(typeof "um texto");
+console.log(typeof 'mais um texto');
+
