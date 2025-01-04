5152fa412124a3ca6264ec923986acb8f7a436db
elivan-alencar <elivanalencar@hotmail.com>
2025-01-04 17:46:42 -0300

    funções atualizada

---
+++ JAVASCRIPT-INFO/ARRAYS E OBJETOS/js/script.js
@@ -0,0 +1,38 @@
+// arrays e bjetos
+
+// arrays
+
+const lista = [0, 1, 2, 3, 4,];
+
+console.log(lista);
+
+
+// acessando elementos do array. arrays dentro de arrays
+
+const itens = [0, 1, 2, 3, 4,[]];
+console.log(itens);
+
+
+// propriedades. 
+
+// length mostra quantos elementos tem no array
+
+const numbers = [4, 5, 7];
+
+console.log(numbers.length);
+
+
+// mostra quantos caracteres tem em uma string
+
+const myName = "Elivan";
+
+console.log(myName.length);
+
+
+// acessando elementos do array
+
+const arr = ["a", "b", "c", "d", "e"];
+
+console.log(arr[3]);
+
+console.log([4]);
