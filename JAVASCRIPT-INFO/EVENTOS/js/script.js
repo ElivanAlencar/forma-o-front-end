2b196771b38ec07e07c7bd237bd98fc1195d7ab2
ElivanAlencar <elivanalencar@hotmail.com>
2025-01-21 17:04:38 -0300

    eventos finalizados

--- JAVASCRIPT-INFO/EVENTOS/js/script.js
+++ JAVASCRIPT-INFO/EVENTOS/js/script.js
@@ -51,4 +51,56 @@ a.addEventListener("click", (e) => {
     e.preventDefault();
 
     console.log("não alterou apágina");
+});
+
+// eventos de tecla
+
+document.addEventListener("keyup", (e) => {
+    console.log(`soltou a telca ${e.key}`);
+});
+
+
+document.addEventListener("keydown", (e) => {
+    console.log(`apertou a telca ${e.key}`);
 })
+
+// eventos de mouse
+
+const mouseEvents = document.querySelector("#mouse");
+
+mouseEvents.addEventListener("mousedown", () => {
+  console.log("pressionou o botão");
+});
+
+mouseEvents.addEventListener("mouseup", () => {
+  console.log("soltou o botao");
+});
+
+mouseEvents.addEventListener("dblclick", () => {
+  console.log("click duplo");
+});
+
+// movimento do mouse
+
+document.addEventListener("mousemove", (e) => {
+  // console.log(`no eixo x: ${e.x}`)
+  // console.log(`no eixo y: ${e.y}`)
+});
+
+
+// evento de scroll
+
+window.addEventListener("scroll", (e) => {
+  if (window.pageYOffset > 200) {
+    console.log("passamos de 200px")
+  }
+});
+
+const input = document.querySelector("#my-input")
+  input.addEventListener("focus", (e) => {
+    console.log("entrou no input");
+  })
+
+
+
+
